f1b9edb54d48298faf20f8f738b0ecc70a505705
rhiaro <amy@rhiaro.co.uk>
2015-11-13 14:50:03 -0500

    auth

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
 include "top.html";
-$nuh = array(".", "..", ".git", ".htaccess", "end.html", "top.html", "index.php");
-if(isset($_GET['dir']) && is_dir($_GET['dir'])){
-  $cur = $_GET['dir'];
-}else{
-  $cur = "/var/www/";
-}
+
+$nuh = array(".", "..", ".git", ".htaccess", "end.html", "top.html", "index.php", "auth");
+        
+if(isset($_GET['dir']) && is_dir($_GET['dir'])){ $cur = $_GET['dir']; }
+else{ $cur = "/var/www/"; }
 $dirs = scandir($cur);
+
 foreach($dirs as $dir){
   if(!in_array($dir, $nuh)){
     if(is_dir($dir)){
