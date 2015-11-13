ca621099ff49a9dbfd1e18b9e2fa25306ec63831
rhiaro <amy@rhiaro.co.uk>
2015-11-13 15:56:04 -0500

    exclude hide

--- index.php
+++ index.php
@@ -6,8 +6,7 @@ $nuh = array(".", "..", ".git", ".htaccess", "end.html", "top.html", "index.php"
 if(isset($_GET['dir']) && is_dir($_GET['dir'])){
   $cur = $_GET['dir'];
   $nuh[] = $cur.".json";
-  $meta = json_decode(file_get_contents($cur."/".$cur.".json"));
-  var_dump($meta);
+  $meta = json_decode(file_get_contents($cur."/".$cur.".json"), true);
 }else{ $cur = "/var/www/"; }
 $dirs = scandir($cur);
 
@@ -18,9 +17,11 @@ foreach($dirs as $dir){
       <li><a href="?dir=<?=$dir?>"><?=$dir?></a></li>
   <?php
     }else{
-      ?>
-      <p><img src="<?=$cur?>/<?=$dir?>" width="200px" /> <?=$dir?></p>
-      <?php
+      if(isset($meta['hide']) && !in_array($dir, $meta['hide'])){
+        ?>
+        <p><img src="<?=$cur?>/<?=$dir?>" width="200px" /> <?=$dir?></p>
+        <?php
+      }
     }
   }
 }
