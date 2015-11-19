373e440f0377d1dc38526fa24afe62bfe1f594ca
rhiaro <amy@rhiaro.co.uk>
2015-11-19 17:11:19 -0500

    debug

--- index.php
+++ index.php
@@ -3,6 +3,8 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+echo phpversion();
+
 function make_json($dir, $date=null, $name="Album"){
   $id = "http://img.amy.gy/".$dir;
   if(!isset($date)){
