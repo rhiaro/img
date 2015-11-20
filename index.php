245da9b8966ba24bbb6b57cf2bd28b24d1092c07
rhiaro <amy@rhiaro.co.uk>
2015-11-20 10:22:31 -0500

    JSON indent

--- index.php
+++ index.php
@@ -32,6 +32,57 @@ function make_json($dir, $date=null, $name="Album"){
   return stripslashes(json_encode($json));
 }
 
+function indent($json) {
+
+    $result      = '';
+    $pos         = 0;
+    $strLen      = strlen($json);
+    $indentStr   = '  ';
+    $newLine     = "\n";
+    $prevChar    = '';
+    $outOfQuotes = true;
+
+    for ($i=0; $i<=$strLen; $i++) {
+
+        // Grab the next character in the string.
+        $char = substr($json, $i, 1);
+
+        // Are we inside a quoted string?
+        if ($char == '"' && $prevChar != '\\') {
+            $outOfQuotes = !$outOfQuotes;
+
+        // If this character is the end of an element,
+        // output a new line and indent the next line.
+        } else if(($char == '}' || $char == ']') && $outOfQuotes) {
+            $result .= $newLine;
+            $pos --;
+            for ($j=0; $j<$pos; $j++) {
+                $result .= $indentStr;
+            }
+        }
+
+        // Add the character to the result string.
+        $result .= $char;
+
+        // If the last character was the beginning of an element,
+        // output a new line and indent the next line.
+        if (($char == ',' || $char == '{' || $char == '[') && $outOfQuotes) {
+            $result .= $newLine;
+            if ($char == '{' || $char == '[') {
+                $pos ++;
+            }
+
+            for ($j = 0; $j < $pos; $j++) {
+                $result .= $indentStr;
+            }
+        }
+
+        $prevChar = $char;
+    }
+
+    return $result;
+}
+
 function get_meta($dir){
   $json = "files/$dir/$dir.json";
   if(file_exists($json)){
@@ -52,7 +103,7 @@ if(isset($_GET['dir']) && $_GET['dir'] != "" && is_dir($root."/".$_GET['dir'])){
     // Create metadata file for first time
     $j = make_json($cur);
     $fp = fopen("files/$cur/$cur.json", 'w');
-    fwrite($fp, $j);
+    fwrite($fp, indent($j));
     fclose($fp);
     $meta = json_decode($j);
   }
