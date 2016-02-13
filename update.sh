c09a526d8a8bf54a82c31da399dba071ed7d4555
rhiaro <amy@rhiaro.co.uk>
2016-02-13 14:11:12 -0500

    Resize files on create

--- update.sh
+++ update.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 while true; do
-  file=$(inotifywait -r --format "%w" -e create -e delete ./*)
+  file=$(inotifywait -r --format "%w%f" -e create -e delete ./*)
+  mogrify -resize '800x600>' $file
   python img.py $file
 done
