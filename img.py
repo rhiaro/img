50d4010db6ef7910a228fff7ace0c5653134e09f
rhiaro <amy@rhiaro.co.uk>
2016-02-13 13:51:45 -0500

    Better dynamic handling of path to images

--- img.py
+++ img.py
@@ -22,32 +22,35 @@ def init_json(path, name, date):
   
   files = [f for f in listdir(path) if isfile(join(path, f))]
   for file in files:
-    data["as2:items"].append({ "@id" : "http://img.amy.gy/files/%s/%s" % (path, file), "as2:name" : "" })
+    data["as2:items"].append({ "@id" : "http://img.amy.gy/%s/%s" % (path, file), "as2:name" : "" })
     
   return data
   
 def update_items(path):
-  with open('%s/%s.json' % (path, path)) as data_file:
+  with open('%s/%s/%s.json' % (path[0], path[1], path[1])) as data_file:
       data = json.load(data_file)
-  files = [f for f in listdir(path) if isfile(join(path, f))]
+  files = [f for f in listdir('%s/%s' % (path[0], path[1])) if isfile(join('%s/%s' % (path[0], path[1]), f))]
   for file in files:
-    if file[-5:] != '.json' and not any([("@id", "http://img.amy.gy/files/%s/%s" % (path, file)) in d.items() for d in data["as2:items"]]):
-      data["as2:items"].append({ "@id" : "http://img.amy.gy/files/%s/%s" % (path, file), "as2:name" : "" })
+    if file[-5:] != '.json' and not any([("@id", "http://img.amy.gy/%s/%s" % ('%s/%s' % (path[0], path[1]), file)) in d.items() for d in data["as2:items"]]):
+      data["as2:items"].append({ "@id" : "http://img.amy.gy/%s/%s" % ('%s/%s' % (path[0], path[1]), file), "as2:name" : "" })
     
   return data
 
 def main(fullpath):
   now = strftime("%Y-%m-%dT%H:%M:%S")
+  print fullpath
   fullpath = fullpath.split('/')
-  path = fullpath[len(fullpath)-2]
+  print fullpath
+  path = (fullpath[len(fullpath)-3], fullpath[len(fullpath)-2])
+  print path
   try:
     data = update_items(path)
     print 'updated'
   except IOError:
-    data = init_json(path, path, now)
+    data = init_json('%s/%s' % (path[0], path[1]), path[1], now)
     print 'new'
   
-  with open('%s/%s.json' % (path, path), 'w') as file:
+  with open('%s/%s/%s.json' % (path[0], path[1], path[1]), 'w') as file:
     file.write(json.dumps(data, indent=4, separators=(',', ': ')))
 
 main(sys.argv[1])
