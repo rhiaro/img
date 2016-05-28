3c596432f9c5f903abce49c8ea5e2e4eb905f7d3
rhiaro <amy@rhiaro.co.uk>
2016-05-28 12:55:52 -0400

    Fix contexts and aliases

--- index.php
+++ index.php
@@ -3,84 +3,36 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+include 'helpers.php';
+
 function make_json($dir, $date=null, $name="Album"){
   $id = "http://img.amy.gy/".$dir;
   if(!isset($date)){
     $date = date(DATE_ATOM);
   }
   $json = array(
-      "@context" => array(
-           "as2" => "http://www.w3.org/ns/activitystreams#"
-          ,"col" => "http://ns.jasnell.me/socialwg#"
-          ,"dc" => "http://purl.org/dc/elements/1.1/"
-          ,"img" => "http://img.amy.gy/v#"
-        ),
-      "@id" => $id,
-      "@type" => array("as2:Collection", "col:Album"),
-      "as2:name" => $name,
-      "as2:published" => $date,
+      "@context" => array("http://www.w3.org/ns/activitystreams#",
+        array(
+             "id" => "@id"
+            ,"type" => "@type"
+            ,"col" => "http://ns.jasnell.me/socialwg#"
+            ,"dc" => "http://purl.org/dc/elements/1.1/"
+            ,"img" => "http://img.amy.gy/v#"
+          )),
+      "id" => $id,
+      "type" => array("Collection", "col:Album"),
+      "name" => $name,
+      "published" => $date,
       "dc:creator" => array("@id" => "http://rhiaro.co.uk/about#me"),
-      "as2:items" => array()
+      "items" => array()
     );
   $files = scandir("files/".$dir);
   foreach($files as $file){
     if(!is_dir("files/".$dir."/".$file)){
-      $json["as2:items"][] = array("@id" => "http://img.amy.gy/files/".$dir."/".$file, "as2:name" => "");
+      $json["items"][] = array("id" => "http://img.amy.gy/files/".$dir."/".$file, "name" => "");
     }
   }
-  
-  return stripslashes(json_encode($json));
-}
-
-function indent($json) {
-
-    $result      = '';
-    $pos         = 0;
-    $strLen      = strlen($json);
-    $indentStr   = '  ';
-    $newLine     = "\n";
-    $prevChar    = '';
-    $outOfQuotes = true;
-
-    for ($i=0; $i<=$strLen; $i++) {
-
-        // Grab the next character in the string.
-        $char = substr($json, $i, 1);
-
-        // Are we inside a quoted string?
-        if ($char == '"' && $prevChar != '\\') {
-            $outOfQuotes = !$outOfQuotes;
-
-        // If this character is the end of an element,
-        // output a new line and indent the next line.
-        } else if(($char == '}' || $char == ']') && $outOfQuotes) {
-            $result .= $newLine;
-            $pos --;
-            for ($j=0; $j<$pos; $j++) {
-                $result .= $indentStr;
-            }
-        }
-
-        // Add the character to the result string.
-        $result .= $char;
-
-        // If the last character was the beginning of an element,
-        // output a new line and indent the next line.
-        if (($char == ',' || $char == '{' || $char == '[') && $outOfQuotes) {
-            $result .= $newLine;
-            if ($char == '{' || $char == '[') {
-                $pos ++;
-            }
-
-            for ($j = 0; $j < $pos; $j++) {
-                $result .= $indentStr;
-            }
-        }
-
-        $prevChar = $char;
-    }
-
-    return $result;
+  return json_encode_pretty($json);
 }
 
 function get_meta($dir){
