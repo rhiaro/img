fc4acc86d51a1aeb4699143030e74bcbe3cc8901
rhiaro <amy@rhiaro.co.uk>
2016-05-30 18:25:54 -0400

    Fix nested albums

--- data.php
+++ data.php
@@ -3,9 +3,13 @@ header("Content-Type: application/activity+json");
 $root = "files";
 if(isset($_GET['dir'])){
   $cur = $_GET['dir'];
-  $json = $root."/".$cur."/".$cur.".json";
+  $fp = explode("/", $cur);
+  $fn = array_pop($fp);
+  $json = "$root/$cur/$fn.json";
   if(file_exists($json)){
     echo file_get_contents($json);
+  }else{
+    echo "Couldn't find data in $json\n";
   }
 }
 ?>
